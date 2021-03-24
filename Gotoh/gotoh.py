a675ff9e39b9a4950e581a23578c45321b22a799
shubh2565 <catchshubham2565@gmail.com>
2021-03-24 09:35:20 +0100

    added functions for matrix initializations

--- Gotoh/gotoh.py
+++ Gotoh/gotoh.py
@@ -76,3 +76,79 @@ def read_fasta_file(fasta_file):
         #remove the first line which is not part of the sequence
         sequence = lines[1]
     return sequence
+ 
+    
+def read_substitution_matrix(file_substitution_matrix):
+    """
+    Implement reading the scores file.
+    It can be stored as a dictionary of example:
+    scores[("A", "R")] = -1
+    """
+    scores = {}
+    with open(file_substitution_matrix) as f:
+        #to get only the lines belonging to scoring matrix
+        lines = [line.split() for line in f if not line.startswith('#') and not line.startswith('\n')]
+    
+    #coverting list into a dictionary
+    for i in range(21):
+        for j in range(1, 22):
+            scores[(lines[j][0], lines[0][i])] = int(lines[j][i+1])
+    
+    return scores
+    
+    
+def init_matrix_d(seq_1, seq_2, cost_gap_open, cost_gap_extend):
+    """
+    Implement initialization of the matrix D
+    """
+    matrix_d = []
+    
+    while len(matrix_d) < len(seq_1)+1:
+        matrix_d.append([])
+        while len(matrix_d[-1]) < len(seq_2)+1:
+            matrix_d[-1].append(0)
+            
+    for i in range(1, len(matrix_d[0])):
+        matrix_d[0][i] = cost_gap_open + cost_gap_extend*(i)
+    for j in range(1, len(matrix_d)):
+        matrix_d[j][0] = cost_gap_open + cost_gap_extend*(j)
+        
+    return matrix_d
+    
+
+def init_matrix_p(seq_1, seq_2):
+    """
+    Implement initialization of the matrix P
+    """
+    matrix_p = []
+    
+    while len(matrix_p) < len(seq_1)+1:
+        matrix_p.append([])
+        while len(matrix_p[-1]) < len(seq_2)+1:
+            matrix_p[-1].append(0)
+    
+    for i in range(1, len(matrix_p[0])):
+        matrix_p[0][i] = -float('Inf')
+    for j in range(1, len(matrix_p)):
+        matrix_p[j][0] = float('NaN')       
+    
+    return matrix_p
+    
+    
+def init_matrix_q(seq_1, seq_2):
+    """
+    Implement initialization of the matrix Q
+    """
+    matrix_q = []
+    
+    while len(matrix_q) < len(seq_1)+1:
+        matrix_q.append([])
+        while len(matrix_q[-1]) < len(seq_2)+1:
+            matrix_q[-1].append(0)
+    
+    for i in range(1, len(matrix_q[0])):
+        matrix_q[0][i] = float('NaN')
+    for j in range(1, len(matrix_q)):
+        matrix_q[j][0] = -float('Inf')
+        
+    return matrix_q
